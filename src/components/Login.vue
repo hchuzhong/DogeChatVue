d152752f6f9c941cba8130b322782b39351ca224
hchuzhong <hchuzhong@CZ-s-MacBook-Air.local>
2022-06-19 17:03:27 +0800

    初步完成登陆页面

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -3,6 +3,15 @@ import {mapActions, mapState, mapStores} from "pinia";
 import {useCounterStore} from "../store/module/counter";
 
 export default {
+    data() {
+        return {
+            form: {
+                username: "",
+                password: "",
+                confirmPassword: ""
+            }
+        };
+    },
     computed: {
         ...mapStores(useCounterStore),
         ...mapState(useCounterStore, ["count"])
@@ -11,6 +20,9 @@ export default {
         ...mapActions(useCounterStore, ["increment"]),
         actionAdd() {
             this.increment();
+        },
+        submit() {
+            console.log("登陆按钮被点击了 ==== ");
         }
     }
 };
@@ -18,8 +30,26 @@ export default {
 
 <template>
     <div>
-        <span>this is a login page</span>
-        <div>{{ count }}</div>
-        <button @click="increment()">add 1</button>
+        <div className="h-screen bg-indigo-100 flex justify-center items-center">
+            <div className="lg:w-2/5 md:w-1/2 w-2/3">
+                <form className="bg-white p-10 rounded-lg shadow-lg min-w-full">
+                    <h1 className="text-center text-2xl mb-6 text-gray-600 font-bold font-sans">登录</h1>
+                    <div>
+                        <label className="text-gray-800 font-semibold block my-3 text-md" htmlFor="username"> Username </label>
+                        <input id="username" v-model="form.username" className="w-full bg-gray-100 px-4 py-2 rounded-lg focus:outline-none" type="text" name="username" placeholder="用户名" />
+                    </div>
+                    <div>
+                        <label className="text-gray-800 font-semibold block my-3 text-md" htmlFor="password"> Password </label>
+                        <input id="password" v-model="form.password" className="w-full bg-gray-100 px-4 py-2 rounded-lg focus:outline-none" type="password" name="password" placeholder="密码" />
+                    </div>
+                    <button type="submit" className="w-full mt-6 bg-indigo-600 rounded-lg px-4 py-2 text-lg text-white tracking-wide font-semibold font-sans" @click="submit">登录</button>
+                    <span className="text-sm ml-2 hover:text-blue-500 cursor-pointer" @click="forget"> Forgot Password ? </span>
+                    <div className="text-center mt-12">
+                        <span>Don't have an account?</span>
+                        <a href="#" className="text-md text-indigo-600 underline font-semibold hover:text-indigo-800"> Create One </a>
+                    </div>
+                </form>
+            </div>
+        </div>
     </div>
 </template>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,13 @@
 import {createRouter, createWebHashHistory} from "vue-router";
 import Login from "../components/Login.vue";
-import Test from "../components/Test.vue";
+import Register from "../components/Register.vue";
+import FriendList from "../components/FriendList.vue";
 
 const routes = [
     {path: "/", component: Login},
-    {path: "/test", component: Test}
+    {path: "/login", component: Login},
+    {path: "/register", component: Register},
+    {path: "/friendlist", component: FriendList}
 ];
 
 const router = createRouter({
