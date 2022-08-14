924cd1b966830a5bf8f816954f197eb9da0dfde1
hchuzhong <huangchuzhong@nowcoder.com>
2022-08-14 21:16:57 +0800

    加上图片懒加载

--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,6 @@ import App from './App.vue';
 import './index.css';
 import router from './router';
 import piniaStore from './store';
+import VueLazyload from 'vue-lazyload';
 
-createApp(App).use(router).use(piniaStore).mount('#app');
+createApp(App).use(router).use(piniaStore).use(VueLazyload).mount('#app');
