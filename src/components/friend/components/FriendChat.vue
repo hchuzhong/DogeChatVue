67598899334816ebe7136429e8e8b2ddd25a5176
hchuzhong <hchuzhong@163.com>
2024-06-01 17:12:44 +0800

    remove function of drag messageItem to repeat message

--- src/components/friend/components/FriendChat.vue
+++ src/components/friend/components/FriendChat.vue
@@ -45,12 +45,6 @@ type dataType = {
     contextMenuX: number;
     contextMenuY: number;
     clickMessageInfo: undefined | FriendMessageType;
-    isDragging: boolean;
-    dragMessageInfo: undefined | FriendMessageType;
-    offsetPosition: PositionType;
-    currentPosition: PositionType;
-    dragDomRect: null | DomRectType;
-    chatDomRect: null | DomRectType;
     isBottom: boolean;
     touch: TouchType;
 };
@@ -93,12 +87,12 @@ export default {
             contextMenuX: 0,
             contextMenuY: 0,
             clickMessageInfo: undefined,
-            isDragging: false,
-            dragMessageInfo: undefined,
-            offsetPosition: {x: 0, y: 0},
-            currentPosition: {x: 0, y: 0},
-            dragDomRect: null,
-            chatDomRect: null,
+            // isDragging: false,
+            // dragMessageInfo: undefined,
+            // offsetPosition: {x: 0, y: 0},
+            // currentPosition: {x: 0, y: 0},
+            // dragDomRect: null,
+            // chatDomRect: null,
             isBottom: true,
             touch: {isTouching: false, startPosition: {x: 0, y: 0}}
         };
@@ -192,67 +186,14 @@ export default {
             this.contextMenuX = target.clientX - (this.isMobile ? 40 : 320);
             this.contextMenuY = target.clientY + chat.scrollTop - 60 - (this.isMobile ? 20 : 0);
             this.clickMessageInfo = messageInfo;
-            this.isDragging = false;
         },
         async commandFor(command: string) {
             this.showContextMenu = false;
             if (command === 'quote') EventBus().dispatchEvent(EventName.QuoteMessage, this.clickMessageInfo);
             if (command === 'recall') recallMessage(this.clickMessageInfo);
         },
-        mouseDown(event: MouseEvent, dragMessageInfo: FriendMessageType, index: number) {
-            event.preventDefault();
-            // 点击右键或者 contextmenu 展示时直接返回
-            if (event.button === 2 || this.showContextMenu) return;
-            const messageEl = document.getElementById(`message${index}`);
-            const chatEl = document.getElementById('chat');
-            if (!messageEl || !chatEl) return console.warn('can not find messafe item', index, ' or chat element');
-            this.dragDomRect = messageEl.getBoundingClientRect();
-            this.chatDomRect = chatEl.getBoundingClientRect();
-
-            const offsetX = event.clientX - this.dragDomRect.x;
-            const offsetY = event.clientY - this.dragDomRect.y;
-
-            this.dragMessageInfo = dragMessageInfo;
-            this.offsetPosition = {x: offsetX, y: offsetY};
-            this.currentPosition = {x: event.clientX, y: event.clientY};
-        },
-        mouseMoveDragMessage(event: MouseEvent) {
-            if (!this.chatDomRect) return;
-            const {clientX, clientY} = event;
-
-            if (!this.isDragging) {
-                const initX = this.currentPosition.x + this.offsetPosition.x;
-                const initY = this.currentPosition.y + this.offsetPosition.y;
-                const minimumDistance = 20;
-                if (Math.max(Math.abs(clientX - initX), Math.abs(clientY - initY)) < minimumDistance) return;
-                this.isDragging = true;
-            }
-            const {x, y, width, height} = this.chatDomRect;
-            if (clientX < x || clientX > x + width || clientY < y || clientY > y + height) {
-                this.resetDragAbout();
-                return toast('拖拽范围仅限于消息列表范围内');
-            }
-            this.currentPosition = {x: event.clientX - this.offsetPosition.x, y: event.clientY - this.offsetPosition.y};
-        },
-        mouseUpDragMessage(event: MouseEvent) {
-            if (!this.dragDomRect || !this.chatDomRect || !this.dragMessageInfo) return;
-            const {clientX, clientY} = event;
-            if (clientX < this.chatDomRect.x || clientX > this.chatDomRect.x + this.chatDomRect.width || clientY < this.chatDomRect.y || clientY > this.chatDomRect.y + this.chatDomRect.height) return this.resetDragAbout();
-            if (clientY < this.dragDomRect.y + this.dragDomRect.height && clientY > this.dragDomRect.y) return this.resetDragAbout();
-            this.repeatMessage(this.dragMessageInfo);
-            this.resetDragAbout();
-        },
         repeatMessage(messafeInfo: FriendMessageType) {
             (this.$refs.friendChatInput as typeof FriendChatInput).sendMessage(messafeInfo?.messageContent, messafeInfo?.type);
-        },
-        resetDragAbout() {
-            this.isDragging = false;
-            this.dragMessageInfo = undefined;
-            this.dragDomRect = null;
-            this.chatDomRect = null;
-            this.dragMessageInfo = undefined;
-            this.offsetPosition = {x: 0, y: 0};
-            this.currentPosition = {x: 0, y: 0};
         },
         touchStart(event: TouchEvent, messageInfo: FriendMessageType) {
             if (!this.isMobile || this.showContextMenu) return;
@@ -283,7 +224,7 @@ export default {
             <UserInfoItem :isLoading="isLoading" :showLoading="true" :userInfo="curChooseFriendInfo" class="justify-center border-b border-gray-400 py-2" />
             <div v-if="!!messageRecords" id="chat" ref="chat" class="w-full h-self-screen overflow-y-auto py-2 px-4 relative" @scroll="scrollChat">
                 <ul>
-                    <MessageItem v-for="(message, index) in messageRecords" :id="`message${index}`" :key="message.uuid" :isSelf="isSelf(message.messageSenderId)" :message="message" :hideIcon="index > 0 && messageRecords[index - 1].messageSenderId === message.messageSenderId" :isMobile="isMobile" @contextmenu="event => showSelfContextMenu(event, message, true)" @mousedown="event => mouseDown(event, message, index)" @touchstart="event => touchStart(event, message)" @touchend="() => (touch.isTouching = false)" @touchmove="touchMove" @repeatMessage="repeatMessage"/>
+                    <MessageItem v-for="(message, index) in messageRecords" :id="`message${index}`" :key="message.uuid" :isSelf="isSelf(message.messageSenderId)" :message="message" :hideIcon="index > 0 && messageRecords[index - 1].messageSenderId === message.messageSenderId" @contextmenu="event => showSelfContextMenu(event, message, true)" @touchstart="event => touchStart(event, message)" @touchend="() => (touch.isTouching = false)" @touchmove="touchMove" @repeatMessage="repeatMessage"/>
                 </ul>
                 <OnClickOutside @trigger="showContextMenu = false">
                     <div v-if="showContextMenu" class="absolute z-10 w-20 max-h-40 border-2 dark:border-gray-300 rounded-lg p-2 border-solid shadow bg-white/[0.8] dark:bg-gray-800/[0.8] overflow-y-auto" :class="{'cannotselect': isMobile}" :style="`top: ${contextMenuY}px; left: ${contextMenuX}px;`">
@@ -292,10 +233,6 @@ export default {
                 </OnClickOutside>
             </div>
             <div v-else class="h-self-screen m-auto text-center text-gray-600 dark:text-gray-400">暂无数据</div>
-
-            <div class="absolute z-20" :style="{top: `${currentPosition.y}px`, left: `${currentPosition.x}px`, width: `${dragDomRect?.width ?? 10}px`, height: `${dragDomRect?.height ?? 10}px`}" @mouseup="mouseUpDragMessage" @mousemove="mouseMoveDragMessage">
-                <MessageItem v-if="dragMessageInfo && isDragging" class="cursor-move" :isSelf="isSelf(dragMessageInfo.messageSenderId)" :message="dragMessageInfo" />
-            </div>
 
             <div class="sticky bottom-0">
                 <FriendChatInput ref="friendChatInput" :chooseFriendInfo="curChooseFriendInfo" :groupMembersData="groupMembersData" :chooseItemId="chooseItemId" @showEmoji="updateScrollPosition" />
